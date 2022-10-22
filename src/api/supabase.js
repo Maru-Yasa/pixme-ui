ed387f178ec580c27d8165955534718147661bce
Maru-Yasa <marufilyasa13@gmail.com>
2022-10-22 08:18:27 +0700

    fix environtment

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,2 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLIC_KEY)
+const url = 'https://jhbwynfypxidtbghkswh.supabase.co'
+const pub_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoYnd5bmZ5cHhpZHRiZ2hrc3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU4NzY1MDUsImV4cCI6MTk4MTQ1MjUwNX0.UagkY4-uGcteinrbobKeOVIh8FSg8DBG8CkzOLgBb1w'
+export const supabase = createClient(url, pub_key)
