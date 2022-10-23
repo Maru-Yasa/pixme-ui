5ab541fb315d853e8026f40a5d132c8e470bdaa3
Maru-Yasa <marufilyasa13@gmail.com>
2022-10-23 09:05:03 +0700

    fix blank profile issue

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -45,36 +45,48 @@ export const Profile = () => {
     }
 
     return <>
-        <div className="flex flex-col justify-center mb-10 p-10">
-            <Board className={'flex flex-col bg-white'}>
-                <h1 className="text-3xl text-center">Hello, {auth.profile.username ? auth.profile.username : <>
-                    <span className="text-red-500">please edit your username</span>
-                </>}</h1>
-                <div className="mb-3 text-center">
-                    <span className="text-green-500">{isCopied && 'Link copied to clipboard'}</span>
-                </div>
-                <div className="flex items-stretch gap-1 justify-center">
-                    <Anchor href={'/edit-profile'} className={'bg-yellow-300 hover:bg-yellow-200 flex items-center'}>Edit Profile</Anchor>
-                    <Anchor href={`/form?user=${auth.data.user.id}`} className="bg-yellow-200 hover:bg-yellow-100 flex items-center">Your form</Anchor>
-                    <CopyToClipboard text={`${location.origin}/form?user=${auth.data.user.id}`} onCopy={() => setIsCopied(true)}>
-                        <Button className={'bg-green-400 hover:bg-green-300'}><i className="bi bi-share-fill"></i> copy link</Button>
-                    </CopyToClipboard>
-                    <Button className={'bg-red-500 hover:bg-red-400 flex items-center'} onClick={handleLogout}>Logout</Button>
-                </div>
-            </Board>
 
-            <div className="mt-3">
-                {isLoading ? <>
-                    <Spinner />
-                </> : <>
-                    <div className="grid grid-cols-2 md:grid-cols-4 mt-3 content-center gap-5">
-                        {messages && messages.map((data) => {
-                            return <Message callBack={handleDeleteCallback} className="" key={data.id} data={data} />
-                        })}
+        {auth.profile ? <>
+            <div className="flex flex-col justify-center mb-10 p-10">
+                <Board className={'flex flex-col bg-white'}>
+                    <h1 className="text-3xl text-center">Hello, {auth.profile.username ? auth.profile.username : <>
+                        <span className="text-red-500">please edit your username</span>
+                    </>}</h1>
+                    <div className="mb-3 text-center">
+                        <span className="text-green-500">{isCopied && 'Link copied to clipboard'}</span>
                     </div>
-                </>}
-            </div>
+                    <div className="flex items-stretch gap-1 justify-center">
+                        <Anchor href={'/edit-profile'} className={'bg-yellow-300 hover:bg-yellow-200 flex items-center'}>Edit Profile</Anchor>
+                        <Anchor href={`/form?user=${auth.data.user.id}`} className="bg-yellow-200 hover:bg-yellow-100 flex items-center">Your form</Anchor>
+                        <CopyToClipboard text={`${location.origin}/form?user=${auth.data.user.id}`} onCopy={() => setIsCopied(true)}>
+                            <Button className={'bg-green-400 hover:bg-green-300'}><i className="bi bi-share-fill"></i> copy link</Button>
+                        </CopyToClipboard>
+                        <Button className={'bg-red-500 hover:bg-red-400 flex items-center'} onClick={handleLogout}>Logout</Button>
+                    </div>
+                </Board>
 
-        </div>
+                <div className="mt-3">
+                    {isLoading ? <>
+                        <Spinner />
+                    </> : <>
+                        <div className="grid grid-cols-2 md:grid-cols-4 mt-3 content-center gap-5">
+                            {messages && messages.map((data) => {
+                                return <Message callBack={handleDeleteCallback} className="" key={data.id} data={data} />
+                            })}
+                        </div>
+                    </>}
+                </div>
+
+            </div>
+        
+        
+        </> : <>
+            <div className="flex px-10 flex-col h-full items-center justify-center">
+                <h1 className="text-center">I'm sorry, something went wrong, u can logout and login again with goolge/email depends on what are u chose at first register/login</h1>
+                <Button className={'bg-red-500 hover:bg-red-400 flex items-center mt-5 shadow-xl hover:shadow-md'} onClick={handleLogout}>Logout</Button>
+            </div>
+        
+        </>}
+
     </>
 }
